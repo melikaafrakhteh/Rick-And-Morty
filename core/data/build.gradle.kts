0263ac01a4a376c8ae6646d65af4b0cf3e0067e9
Melika Afrakhteh <melikaafrakhteh@gmail.com>
2024-09-13 16:53:43 +0330

    Update data build gradle file

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -1,10 +1,21 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.kotlin.serialization)
+    kotlin("kapt")
 }
 
 android {
     namespace = "com.melika.rickandmorty.core.data"
 }
 
-dependencies {}
+dependencies {
+    implementation(project(":core:model"))
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+}
